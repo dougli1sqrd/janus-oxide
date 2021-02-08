04074ce382d143c8c548f702ea1d2bbda108628b
dougli1sqrd <dougli1sqrd+git@gmail.com>
2021-02-07 20:29:52 -0800

    Transact trait to unify the different store types in oxigraph

--- src/api/model.rs
+++ src/api/model.rs
@@ -5,25 +5,40 @@
 use sophia_api::dataset::Dataset;
 
 use oxigraph::MemoryStore;
-
+use oxigraph::store::memory::{MemoryTransaction};
 use oxigraph::SledStore;
 use oxigraph::store::sled::SledTransaction;
 use oxigraph::store::sled::SledTransactionError;
 use oxigraph::store::sled::SledConflictableTransactionError;
 
+
+use std::convert::Infallible;
+
 struct Store<S: Dataset> {
     store: S
 }
 
-trait Transact<T, I, E> {
-    fn transaction(&self, f: impl FnOnce(T) -> Result<(), I>) -> Result<(), E>;
+trait Transact<F, T, R, I, E> 
+    where 
+        F: Fn(T) -> Result<R, I> {
+
+    fn transaction(&self, f: F) -> Result<R, E>;
 }
 
+impl<F, T> Transact<F, SledTransaction<'_>, T, SledConflictableTransactionError<Infallible>, SledTransactionError<Infallible>> for SledStore
+    where
+        F: Fn(SledTransaction<'_>) -> Result<T, SledConflictableTransactionError<Infallible>> {
 
-impl<'r, E> Transact<SledTransaction<'r>, SledConflictableTransactionError<E>, SledTransactionError<E>> for SledStore {
-    fn transaction(&self, f: impl FnOnce(SledTransaction<'r>) -> Result<(), SledConflictableTransactionError<E>>) -> Result<(), SledTransactionError<E>> {
-        self.transaction(|transaction: SledTransaction<'r>| {
-            f(transaction)
-        })
+    fn transaction(&self, f: F) -> Result<T, SledTransactionError<Infallible>> {
+        self.transaction(f)
+    }
+}
+
+impl<F, E> Transact<F, &mut MemoryTransaction, (), E, E> for MemoryStore
+    where
+        F: Fn(&mut MemoryTransaction) -> Result<(), E> {
+
+    fn transaction(&self, f: F) -> Result<(), E> {
+        self.transaction(f)
     }
 }
